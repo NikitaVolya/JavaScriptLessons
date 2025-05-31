af7cc149de3bffb5ee35c4a3874e92e9e4b1866a
NikitaVoliqLP <volanskijnikita2@gmail.com>
2025-05-31 10:04:42 +0200

    add Lessong2 and Lesson2.Exerice1

---
+++ Lesson2/main.js
@@ -0,0 +1,45 @@
+
+const obj = new Object();
+
+obj.name = 'Max';
+obj.age = 25;
+
+console.log(obj.name);
+console.log(obj);
+
+delete obj.age;
+
+console.log(obj);
+
+const isExiste = (object) => {
+
+    if ('age' in object) {
+        console.log('Exists');
+    }
+    else {
+        console.log('Not exists');
+    }
+}
+
+
+// Exercice1
+function Exercice1() {
+    let name_input = prompt("Name: ");
+    let age_input = Number(prompt("Age: "));
+    let city_input = prompt("City");
+    let street_input = prompt("street");
+
+    const student = {
+        user: {
+            name: name_input,
+            age: age_input
+        },
+        adress: {
+            city: city_input,
+            street: street_input
+        }
+    }
+    console.log(student);
+}
+
+Exercice1();
