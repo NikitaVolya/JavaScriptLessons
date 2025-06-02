5ad5865f616ab1bf56bdaf0ff4c538221252b9d3
NikitaVoliqLP <volanskijnikita2@gmail.com>
2025-06-02 20:10:01 +0200

    fixed bug. Add multiple input of date formats

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -38,14 +38,15 @@ function isLeapYear(year) {
 function getDays(month, year) {
     if (!isNumber(month) || !isNumber(year))
         return 28;
-    switch (month) {
+    console.log(month);
+    switch (Number(month)) {
         case 4: case 6: case 9: case 11:
             return 30;
         case 1: case 3: case 5: case 7: 
         case 8: case 10: case 12:
             return 31;
         case 2:
-            return isLeapYear(year) ? 29 : 28;
+            return isLeapYear(Number(year)) ? 29 : 28;
         default:
             return 28;
     }
@@ -62,11 +63,20 @@ function checkDate(day, month, year) {
     return rep;
 }
 
+function splitDate(text) {
+    let symbls = ['.', '/', '-', ' '];
+    for (let i in symbls)
+    {
+        if (text.indexOf(symbls[i]) != -1)
+            return text.split(symbls[i]);
+    }
+    return null;
+}
+
 function main() {
-    let input = prompt('Enter your date (like 13/03/2006): ');
-    let data = input.split('/');
+    let data = splitDate(prompt('Enter your date (like 13/03/2006): '));
     
-    if (data.length != 3)
+    if (data == null || data.length != 3)
     {
         alert('Incorrect input format');
         return;
@@ -78,7 +88,7 @@ function main() {
     {
         case checkDateResult.Ok:
             let date = new Date(data[2], data[1] - 1, data[0]);
-            alert(date);
+            alert(date.toDateString());
             break;
         default:
             alert(checkDateResult.ToString(date_rep));
