16e6548f0a3e36d2466902a25b2bf62e03366453
NikitaVoliaLT <volanskijnikita2@gmail.com>
2025-06-14 09:37:05 +0200

    Add change events on all fields in Exercice 1 Lesson 5

--- Lessons/Lesson5/main.js
+++ Lessons/Lesson5/main.js
@@ -1,7 +1,12 @@
 
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const repeatPasswordInput = document.getElementById('passwordValidation');
+
+
 function checkName()
 {
-    const nameInput = document.getElementById('name');
     if (nameInput.value.length < 2)
     {
         nameInput.style.borderBlockColor = 'red';
@@ -15,7 +20,6 @@ function checkName()
 }
 
 function checkEmail() {
-    const emailInput = document.getElementById('email');
     let email = emailInput.value;
     if (!(email.indexOf('@gmail.com') != -1 || email.indexOf('@yahoo.com') != -1) || email.includes(' '))
     {
@@ -30,7 +34,6 @@ function checkEmail() {
 }
 
 function checkPassword() {
-    const passwordInput = document.getElementById('password');
     if (passwordInput.value.length < 6)
     {
         passwordInput.style.borderBlockColor = 'red';
@@ -44,8 +47,6 @@ function checkPassword() {
 }
 
 function checkRepeatPassword() {
-    const passwordInput = document.getElementById('password');
-    const repeatPasswordInput = document.getElementById('passwordValidation');
     if (passwordInput.value != repeatPasswordInput.value)
     {
         repeatPasswordInput.style.borderBlockColor = 'red';
@@ -58,6 +59,10 @@ function checkRepeatPassword() {
     return true;
 }
 
+nameInput.addEventListener('change', () => { checkName();} );
+emailInput.addEventListener('change', () => { checkEmail();} );
+passwordInput.addEventListener('change', () => { checkPassword(); });
+repeatPasswordInput.addEventListener('change', () => { checkRepeatPassword(); });
 
 function RegisterForm() {
     if (checkName() && checkEmail() && checkPassword() && checkRepeatPassword())
