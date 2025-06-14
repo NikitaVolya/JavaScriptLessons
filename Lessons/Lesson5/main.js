dd375dce6580908a4765bee2b8a5f259f33928f5
NikitaVoliaLT <volanskijnikita2@gmail.com>
2025-06-14 11:27:50 +0200

    Refactor checkAllValide function in Lesson 5 to shorted code

--- Lessons/Lesson5/main.js
+++ Lessons/Lesson5/main.js
@@ -3,60 +3,76 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const repeatPasswordInput = document.getElementById('passwordValidation');
+const fields = [nameInput, emailInput, passwordInput, repeatPasswordInput];
 
+function validationErrorMessage(filed, text) {
+    filed.style.borderBlockColor = 'red';
+    filed.parentElement.lastElementChild.textContent = text;
+}
+
+function validationErrorMessageClear(field) {
+    field.style.borderBlockColor = 'canvas';
+    field.parentElement.lastElementChild.textContent = '';
+}
 
 function checkName()
 {
     if (nameInput.value.length < 2)
     {
-        nameInput.style.borderBlockColor = 'red';
-        nameInput.parentElement.lastElementChild.textContent = 'Name must contain at least 2 letters!';
+        validationErrorMessage(nameInput, 'Name must contain at least 2 letters!');
         return false;
     }
 
-    nameInput.style.borderBlockColor = 'canvas';
-    nameInput.parentElement.lastElementChild.textContent = '';
+    validationErrorMessageClear(nameInput);
     return true;
 }
 
 function checkEmail() {
     let email = emailInput.value;
-    if (!(email.indexOf('@gmail.com') != -1 || email.indexOf('@yahoo.com') != -1) || email.includes(' '))
+    if (!(email.indexOf('@gmail.com') != -1 || email.indexOf('@yahoo.com') != -1) || email.includes(' ') || email.indexOf('@') == 0)
     {
-        emailInput.style.borderBlockColor = 'red';
-        emailInput.parentElement.lastElementChild.textContent = 'Email is not valide!';
+        validationErrorMessage(emailInput, 'Email is not valide!');
         return false;
     } 
-    
-    emailInput.style.borderBlockColor = 'canvas';
-    emailInput.parentElement.lastElementChild.textContent = '';
+    validationErrorMessageClear(emailInput);
     return true;
 }
 
 function checkPassword() {
     if (passwordInput.value.length < 6)
     {
-        passwordInput.style.borderBlockColor = 'red';
-        passwordInput.parentElement.lastElementChild.textContent = 'Password must contain at least 6 letters!';
+        validationErrorMessage(passwordInput, 'Password must contain at least 6 letters!');
         return false;
     }
 
-    passwordInput.style.borderBlockColor = 'canvas';
-    passwordInput.parentElement.lastElementChild.textContent = '';
+    validationErrorMessageClear(passwordInput);
     return true;
 }
 
 function checkRepeatPassword() {
     if (passwordInput.value != repeatPasswordInput.value)
     {
-        repeatPasswordInput.style.borderBlockColor = 'red';
-        repeatPasswordInput.parentElement.lastElementChild.textContent = 'Passwords are not the same!';
+        validationErrorMessage(repeatPasswordInput, 'Passwords are not the same!');
         return false;
     }
     
-    repeatPasswordInput.style.borderBlockColor = 'canvas';
-    repeatPasswordInput.parentElement.lastElementChild.textContent = '';
+    validationErrorMessageClear(repeatPasswordInput);
     return true;
+}
+
+function checkFillFields() {
+    let allFill = true;
+    for (i in fields)
+    {
+        if (!fields[i].value)
+            validationErrorMessage(fields[i], 'Required field to fill in')
+        allFill = false;
+    }
+    return allFill;
+}
+
+function checkAllValide() {
+    return !(!checkName() || !checkEmail() || !checkPassword() || !checkRepeatPassword());
 }
 
 nameInput.addEventListener('change', () => { checkName();} );
@@ -65,8 +81,10 @@ passwordInput.addEventListener('change', () => { checkPassword(); });
 repeatPasswordInput.addEventListener('change', () => { checkRepeatPassword(); });
 
 function RegisterForm() {
-    if (checkName() && checkEmail() && checkPassword() && checkRepeatPassword())
-    {
-        alert('Successful register!!!');
-    }
+    if (!checkFillFields())
+        return;
+    if (!checkAllValide())
+        return;
+    
+    alert('Successful register!!!');
 }
